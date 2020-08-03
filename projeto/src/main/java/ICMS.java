981a850177dea2f5d3ed9626c42c704c706ca184
Fabiana Assami <fabiana.assami@wirecard.com>
2020-08-02 22:02:11 -0300

    separada a responsabilidade do calculo - encapsulamento

---
+++ projeto/src/main/java/ICMS.java
@@ -0,0 +1,5 @@
+public class ICMS {
+    public double calculaICMS(Orcamento orcamento) {
+        return orcamento.getValor() * 0.1;
+    }
+}
---
+++ projeto/src/main/java/ISS.java
@@ -0,0 +1,5 @@
+public class ISS {
+    public double calculaISS(Orcamento orcamento) {
+        return orcamento.getValor() * 0.06;
+    }
+}
