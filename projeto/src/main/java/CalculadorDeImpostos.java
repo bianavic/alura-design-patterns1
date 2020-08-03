743b2b48267fc3782806cfe212425101820edfe3
Fabiana Assami <fabiana.assami@wirecard.com>
2020-08-02 21:56:14 -0300

    criada classe com muitas responsabilidades

---
+++ projeto/src/main/java/CalculadorDeImpostos.java
@@ -0,0 +1,14 @@
+public class CalculadorDeImpostos {
+
+    public void realizaCalculo(Orcamento orcamento, String imposto) {
+
+        if (imposto.equals("ICMS")) {
+            double icms = orcamento.getValor() * 0.1;
+            System.out.println(icms);
+        } else if (imposto.equals("ISS")) {
+            double iss = orcamento.getValor() * 0.06;
+            System.out.println(iss);
+        }
+    }
+
+}
