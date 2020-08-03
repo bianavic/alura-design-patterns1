80b43247b1f7b2d950efa471ae43c9daed448d86
Fabiana Assami <fabiana.assami@wirecard.com>
2020-08-02 22:07:33 -0300

    separado metodos e eliminada condicional if

--- projeto/src/main/java/CalculadorDeImpostos.java
+++ projeto/src/main/java/CalculadorDeImpostos.java
@@ -1,14 +1,14 @@
 public class CalculadorDeImpostos {
 
-    public void realizaCalculo(Orcamento orcamento, String imposto) {
+    public void realizaCalculoICMS(Orcamento orcamento) {
 
-        if (imposto.equals("ICMS")) {
             double icms = new ICMS().calculaICMS(orcamento);
             System.out.println(icms);
-        } else if (imposto.equals("ISS")) {
+    }
+
+    public void realizaCalculoISS(Orcamento orcamento) {
+
             double iss = new ISS().calculaISS(orcamento);
             System.out.println(iss);
         }
-    }
-
 }
